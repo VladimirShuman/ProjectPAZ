406a5a11ae054d192433b49e5d22144277864c53
VlShuman <Vladimir.Shuman@gmail.com>
2019-05-19 08:56:59 +0300

    Page Connect

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function onclickContact() {
+	document.getElementById("main-page-home").style.display="none";
+	document.getElementById("main-page-connect").style.display="";
+}
+function onclickHome() {
+	document.getElementById("main-page-home").style.display="";
+	document.getElementById("main-page-connect").style.display="none";
+}
+
